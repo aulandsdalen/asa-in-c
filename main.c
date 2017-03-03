ff3bfd9d6db2b4f8d7cf1362dad236d21891d338
aulandsdalen <aulandsdalen@gmail.com>
2017-03-03 18:44:26 +0300

    fixed allocation error

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <time.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 #define FILE_SEPARATOR ","
 #define NEWLINE "\n"
@@ -11,10 +12,23 @@ struct data {
 	time_t timestamp;
 };
 
+struct execution_data {
+	struct timeval begin;
+	struct timeval end;
+};
+
 int compare_data(const void *a, const void *b) {
 	struct data *A = (struct data*)a;
 	struct data *B = (struct data*)b;
 	return (A->timestamp - B->timestamp);
+}
+
+void start_timer(struct execution_data *ed) {
+	gettimeofday(&ed->begin, NULL);
+}
+
+void stop_timer(struct execution_data *ed) {
+	gettimeofday(&ed->end, NULL);
 }
 
 int main(int argc, char** argv) {
@@ -23,32 +37,30 @@ int main(int argc, char** argv) {
 		return -1;
 	}
 	struct data *data_arr;
-	//data_arr= malloc(sizeof(struct data));
 	int row_number = 0;
 	char *row;
 	FILE *data_file;
+	struct execution_data qsort_ed;
 	if(!(data_file = fopen(argv[1], "r"))) {
 		printf("Error reading from %s, exiting...\n", argv[1]);
 		return 0;
 	}
 	row = (char*)malloc(sizeof(char)*255);
 	while(fgets(row, 255, data_file)) {
-		//fscanf(data_file, "%s", row);
-		data_arr = realloc(data_arr, row_number + (row_number*sizeof(struct data)));
+		data_arr = realloc(data_arr, (sizeof(struct data) * (row_number+1)));
 		data_arr[row_number].measurement = atof(strtok(row, FILE_SEPARATOR));
 		data_arr[row_number].timestamp = atoi(strtok(NULL, NEWLINE));
-		//printf("%2f, %ld\n", data_arr[row_number].measurement, data_arr[row_number].timestamp);
 		row_number++;
 	}
-	printf("%d rows read\n", row_number); 
-	fclose(data_file);
-	struct data a, b;
-	a.measurement = 0.123;	
-	a.timestamp = 345;
-	b.measurement = 0;
-	b.timestamp = 567;
-	printf("now sorting...");
+	printf("%d rows read\n", row_number);
+	start_timer(&qsort_ed);
 	qsort(data_arr, row_number, sizeof(struct data), compare_data);
+	stop_timer(&qsort_ed);
+	time_t seconds = qsort_ed.end.tv_sec - qsort_ed.begin.tv_sec;
+	suseconds_t usec = qsort_ed.end.tv_usec - qsort_ed.begin.tv_usec;
+	printf("%ld sec %d usec\n", seconds, usec);
 	printf("sorted\n");
+	free(data_arr);
+	fclose(data_file);
 	return 0;
 }
